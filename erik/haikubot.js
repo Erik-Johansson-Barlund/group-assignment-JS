d055ca61d8a79b9dfbef1e393ecee368b15cf67c
Erik Johansson Bärlund <erik163@gmail.com>
2020-12-07 17:12:44 +0100

    Uppdaterat min sida

--- erik/haikubot.js
+++ erik/haikubot.js
@@ -1,105 +1,102 @@
-let haikuWrap = document.getElementById("haiku_wrapper");
-let haikuDiv = document.getElementById("haiku_div");
-let haiku = "Den ";
+let haikuDiv = document.getElementById('haiku_div');
+let haiku = 'Den ';
 let haikuArray = [
-  [
-    "gamla",
-    "lilla",
-    "stora",
-    "unga",
-    "blå",
-    "varma",
-    "kalla",
-    "röda",
-    "strävande",
-    "friska",
-    "glada",
-    "sorgsna",
-    "avlägsna",
-  ],
-  [
-    "ängen,<br>",
-    "sjön,<br>",
-    "dammen,<br>",
-    "anden,<br>",
-    "kroppen,<br>",
-    "floden,<br>",
-    "toppen<br>",
-    "stranden,<br>",
-    "bäcken,<br>",
-  ],
-  [
-    "en pojke",
-    "en flicka",
-    "en man",
-    "en kvinna",
-    "en gumma",
-    "en drake",
-    "en evighet",
-    "en apa",
-    "en fisk",
-    "en krigare",
-    "en tanke",
-  ],
-  [
-    "hoppar",
-    "går",
-    "ligger",
-    "skuttar",
-    "sitter",
-    "glider",
-    "flyger",
-    "svävar",
-    "kryper",
-    "springer",
-  ],
-  [
-    "i,<br>",
-    "ur,<br>",
-    "upp,<br>",
-    "ned,<br>",
-    "bort,<br>",
-    "bortom,<br>",
-    "bredvid,<br>",
-    "nedanför,<br>",
-    "på,<br>",
-  ],
-  [
-    "ljudet av",
-    "doften av",
-    "synen av",
-    "förnimmelsen av",
-    "känslan av",
-    "tanken på",
-    "längtan efter",
-    "avsaknaden av",
-    "vördnaden inför",
-    "fruktan inför",
-    "bävan inför",
-  ],
-  [
-    "tiden",
-    "vatten",
-    "snö",
-    "gräs",
-    "jord",
-    "himmelen",
-    "träd",
-    "det förgågna",
-    "historien",
-  ],
+	[
+		'gamla',
+		'lilla',
+		'stora',
+		'unga',
+		'blå',
+		'varma',
+		'kalla',
+		'röda',
+		'strävande',
+		'friska',
+		'glada',
+		'sorgsna',
+		'avlägsna',
+	],
+	[
+		'ängen,<br>',
+		'sjön,<br>',
+		'dammen,<br>',
+		'anden,<br>',
+		'kroppen,<br>',
+		'floden,<br>',
+		'toppen<br>',
+		'stranden,<br>',
+		'bäcken,<br>',
+	],
+	[
+		'en pojke',
+		'en flicka',
+		'en man',
+		'en kvinna',
+		'en gumma',
+		'en drake',
+		'en evighet',
+		'en apa',
+		'en fisk',
+		'en krigare',
+		'en tanke',
+	],
+	[
+		'hoppar',
+		'går',
+		'ligger',
+		'skuttar',
+		'sitter',
+		'glider',
+		'flyger',
+		'svävar',
+		'kryper',
+		'springer',
+	],
+	[
+		'i,<br>',
+		'ur,<br>',
+		'upp,<br>',
+		'ned,<br>',
+		'bort,<br>',
+		'bortom,<br>',
+		'bredvid,<br>',
+		'nedanför,<br>',
+		'på,<br>',
+	],
+	[
+		'ljudet av',
+		'doften av',
+		'synen av',
+		'förnimmelsen av',
+		'känslan av',
+		'tanken på',
+		'längtan efter',
+		'avsaknaden av',
+		'vördnaden inför',
+		'fruktan inför',
+		'bävan inför',
+	],
+	[
+		'tiden.',
+		'vatten.',
+		'snö.',
+		'gräs.',
+		'jord.',
+		'himmelen.',
+		'träd.',
+		'det förgågna.',
+		'historien.',
+	],
 ];
 
 function haikuMaker() {
-  for (let i = 0; i < haikuArray.length; i++) {
-    let x = Math.floor(Math.random() * haikuArray[i].length);
-    if (x > haikuArray[i].length) {
-      x--;
-    }
-    console.log(x);
-    haiku += haikuArray[i][x] + " ";
-  }
-  console.log(haiku);
-  haikuDiv.innerHTML = haiku;
-  haiku = "Den ";
+	for (let i = 0; i < haikuArray.length; i++) {
+		let x = Math.floor(Math.random() * haikuArray[i].length);
+		if (x > haikuArray[i].length) {
+			x--;
+		}
+		console.log(x);
+		haiku += haikuArray[i][x] + ' ';
+	}
+	haikuDiv.innerHTML = haiku;
 }
