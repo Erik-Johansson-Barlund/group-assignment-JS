69a2b28ddbe4a2bbf903dd9b58f003a9e4154e50
Erik Johansson Bärlund <erik163@gmail.com>
2020-12-10 11:34:47 +0100

    Lite design

--- script.js
+++ script.js
@@ -5,24 +5,36 @@ class Game {
 		this.highscore = [];
 		this.player1 = '';
 		this.player2 = '';
+		this.stickArray = [];
 
 		//this.startGame();
 	}
 	removeSticks() {
-		/*
-		  ta bort pinnar grafiskt och sänka counter
-		  kolla om counter är 0 <- anropa endGame
-		*/
-
 		let noOfSticks = 0;
 		console.log('event target id removeStics: ' + event.target.id);
+		/*
 		if (event.target.id == "btn1") { noOfSticks = 1; }
 		if (event.target.id == "btn2") { noOfSticks = 2; }
 		if (event.target.id == "btn3") { noOfSticks = 3; }
+		*/
+		if (event.target.id == 'btn1') {
+			noOfSticks = 1;
+			this.stickArray.pop();
+		}
+		if (event.target.id == 'btn2') {
+			noOfSticks = 2;
+			this.stickArray.splice(this.stickArray.length - 2, 2);
+		}
+		if (event.target.id == 'btn3') {
+			noOfSticks = 3;
+			this.stickArray.splice(this.stickArray.length - 3, 3);
+		}
 		//console.log('No of sticks to remove: ' + noOfSticks);
 		this.counter -= noOfSticks;
+		/*HÄR*/ board.innerHTML = this.counter + ' Stickor kvar';
+		/*HÄR*/ board.innerHTML += this.stickArray.join('');
 		console.log('this counter from removeSticks: ' + this.counter);
-		board.innerHTML = this.counter;
+		//this.printSticks(this.counter);
 		if (this.counter > 1) {
 			this.turn = this.turn ? false : true;
 			if (this.turn) {
@@ -40,35 +52,37 @@ class Game {
 		this.turn = true;
 		this.turn = Math.round(Math.random());
 		console.log('turn ' + this.turn);
-		btnStart.disabled = true; 
-		if (this.player1 == "") {
-			while (this.player1 === "") {
+		btnStart.disabled = true;
+		/*HÄR*/ this.setupSticks();
+		if (this.player1 == '') {
+			while (this.player1 === '') {
 				this.player1 = prompt('Namn för spelare 1');
 			}
 			console.log(this.player1);
-			while (this.player2 === "") {
+			while (this.player2 === '') {
 				this.player2 = prompt('Namn för spelare 2');
 			}
-			if (this.player1 === null || this.player2 === null )
+			if (this.player1 === null || this.player2 === null)
 				location.reload();
 			console.log(this.player2);
 		}
-		
-		if (this.turn)
-			playerText.innerHTML = `${this.player1}s tur`;
-		else
-			playerText.innerHTML = `${this.player2}s tur`;
-		board.innerHTML = this.counter;
+
+		if (this.turn) playerText.innerHTML = `${this.player1}s tur`;
+		else playerText.innerHTML = `${this.player2}s tur`;
+		//this.printSticks(this.counter);
+		/*HÄR*/ board.innerHTML = this.counter + ' Stickor kvar';
+		/*HÄR*/ board.innerHTML += this.stickArray.join('');
 	}
+
 	endGame() {
-		let winner = this.turn ? this.player1 : this.player2
-		board.innerHTML = winner + " vann!";
+		let winner = this.turn ? this.player1 : this.player2;
+		board.innerHTML = winner + ' vann!';
 		let notInHiScore = true;
 		for (let player of this.highscore) {
 			if (player.name == winner) {
 				player.points += 2;
 				notInHiScore = false;
-				console.log("Found player in highscore" + player.points);
+				console.log('Found player in highscore' + player.points);
 				break;
 			}
 		}
@@ -77,25 +91,43 @@ class Game {
 		}
 
 		this.printHighScore();
-		
-		btnStart.disabled = false; 
 
-		let playAgain = confirm("Vill du spela igen?");
-		if (playAgain) 
-			this.startGame();
+		btnStart.disabled = false;
+
+		let playAgain = confirm('Vill du spela igen?');
+		if (playAgain) this.startGame();
 		else {
-			this.player1 = "";
-			this.player2 = ""; 
+			this.player1 = '';
+			this.player2 = '';
 		}
 	}
 
 	printHighScore() {
-		let output = "";
+		let output = '';
 		this.highscore.sort((a, b) => a.points - b.points);
 		for (let player of this.highscore) {
-			output += player.name + ": " + player.points + "<br>";
+			output += player.name + ': ' + player.points + '<br>';
 		}
 		highScoreText.innerHTML = output;
+	}
+	/*
+	printSticks(noOfSticks) {
+		let outputSticks = ``;
+		let stick = `<div class="game__stick"></div>`;
+		for(let i = 1; i<= this.counter; i++){
+			outputSticks += stick;
+		}
+		board.innerHTML = outputSticks;
+	}
+*/
+	setupSticks() {
+		this.stickArray = [];
+		for (let i = 0; i < this.counter; i++) {
+			let stick = `<div class="allsticks" style=" left: ${
+				47 + Math.round(Math.random() * 6)
+			}%; transform: rotate(${Math.random() * 180}deg);"></div>`;
+			this.stickArray.push(stick);
+		}
 	}
 }
 
@@ -118,29 +150,27 @@ document.addEventListener('DOMContentLoaded', function (e) {
 	board = document.getElementById('board');
 	highScoreText = document.getElementById('hiscore');
 	playerText = document.getElementById('playertext');
-	btnStart = document.getElementById("btnStart");
-	btn1 = document.getElementById("btn1");
-	btn2 = document.getElementById("btn2");
-	btn3 = document.getElementById("btn3");
+	btnStart = document.getElementById('btnStart');
+	btn1 = document.getElementById('btn1');
+	btn2 = document.getElementById('btn2');
+	btn3 = document.getElementById('btn3');
 	myGame = new Game();
-	//myGame.startGame(); körs i constructorn 
+	//myGame.startGame(); körs i constructorn
 
-	btnStart.addEventListener("click", () => {
-		console.log("Knapp start");
+	btnStart.addEventListener('click', () => {
+		console.log('Knapp start');
 		myGame.startGame();
 	});
-	btn1.addEventListener("click", () => {
-		console.log("Knapp 1 trycks");
+	btn1.addEventListener('click', () => {
+		console.log('Knapp 1 trycks');
 		myGame.removeSticks();
 	});
-	btn2.addEventListener("click", () => {
-		console.log("Knapp 2 trycks");
+	btn2.addEventListener('click', () => {
+		console.log('Knapp 2 trycks');
 		myGame.removeSticks();
 	});
-	btn3.addEventListener("click", () => {
-		console.log("Knapp 3 trycks");
+	btn3.addEventListener('click', () => {
+		console.log('Knapp 3 trycks');
 		myGame.removeSticks();
 	});
-
 });
-
